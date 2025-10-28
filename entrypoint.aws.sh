b307e05afd193d88d7a9852a436f6284ef997605
Jakub Stribrny <quidible@gmail.com>
2025-10-28 13:50:58 +0100

    moved docker-related files to subdir

--- entrypoint.aws.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-aws s3 cp s3://amzn-s3-bookrecommender/Ratingss.csv /app/Downloads/Ratings.csv
-aws s3 cp s3://amzn-s3-bookrecommender/Bookss.csv /app/Downloads/Books.csv
-exec streamlit run streamlit_app.py --server.port=8501 --server.address=0.0.0.0
